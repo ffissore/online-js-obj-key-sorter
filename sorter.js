302c94d1cc17c43504306ee154f9b0f32938a4bb
Federico Fissore <federico@fissore.org>
2016-08-10 15:37:46 +0200

    Added coffeescript output

--- sorter.js
+++ sorter.js
@@ -1,18 +1,63 @@
 "use strict";
 
+var INDENTATION = 2;
+
 $("#sort").click(function () {
+  var $jsoutput = $("#jsoutput");
+  var $coffeeoutput = $("#coffeeoutput");
+
+  $jsoutput.val("");
+  $coffeeoutput.val("");
   try {
     var source = $("#source").val();
     var object = eval(CoffeeScript.compile(source, {bare: true}));
     var newObject = sort(object);
-    $("#output").val(jsFriendlyJSONStringify(newObject, 2));
+    var js = objToJSSource(newObject, INDENTATION);
+    var coffee = jsSourceToCoffee(js);
+    $jsoutput.val(js);
+    $coffeeoutput.val(coffee);
   } catch (e) {
-    $("#output").val(e);
+    $jsoutput.val(e);
   }
 });
 
-function jsFriendlyJSONStringify(s, spaces) {
-  return JSON.stringify(s, null, spaces).replace(/\u2028/g, "\\u2028").replace(/\u2029/g, "\\u2029").replace(/\"([^(\")"]+)\":/g, "$1:");
+function objToJSSource(s, spaces) {
+  return jsFriendlyJSONStringify(JSON.stringify(s, null, spaces)).replace(/\"([^(\")"]+)\":/g, "$1:");
+}
+
+function jsFriendlyJSONStringify(str) {
+  return str.replace(/\u2028/g, "\\u2028").replace(/\u2029/g, "\\u2029");
+}
+
+function jsSourceToCoffee(source) {
+  return removeCurlyBraces(source)
+      .split("\n")
+      .filter(nonEmptyLines)
+      .map(removeFirstSpaces(INDENTATION))
+      .map(removeTrailingCommas)
+      .join("\n");
+}
+
+function nonEmptyLines(line) {
+  return line.trim() != "";
+}
+
+function removeFirstSpaces(spaces) {
+  return function (line) {
+    return line.substring(spaces);
+  }
+}
+
+function removeTrailingCommas(line) {
+  return line.replace(/,$/, "");
+}
+
+function removeCurlyBraces(str) {
+  return str
+      .replace(/ \{/g, "")
+      .replace(/\},/g, "")
+      .replace(/\{/g, "")
+      .replace(/\}/g, "");
 }
 
 function sort(obj) {
